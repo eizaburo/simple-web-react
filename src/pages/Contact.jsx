9452fe97352b3d217ae1f13d563b39f763a9cff2
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2026-03-20 09:28:31 +0900

    レイアウトは一通り完成

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,20 +4,23 @@ function Contact() {
     return (
         <main>
             <section className={styles.hero}>
-                <h1>ヒーローエリア</h1>
-                <p>ヒーローエリアのキャッチコピー</p>
+                <h1>お問合せフォーム</h1>
+                <p>お気軽にお問合せ下さい</p>
             </section>
             <section className={styles.formContainer}>
                 <form className={styles.contactForm}>
 
                     <label htmlFor='title'>お問合せタイトル</label>
                     <input type='text' id='title' />
+                    <p className={styles.errorMessage}>お問合せタイトルを正しく入力して下さい。</p>
 
                     <label htmlFor='email'>Email</label>
                     <input type='text' id='email' />
+                    <p className={styles.errorMessage}>Emailを正しく入力して下さい。</p>
 
                     <label htmlFor='title'>お問合せ内容</label>
                     <textarea id='message' rows={5}></textarea>
+                    <p className={styles.errorMessage}>お問合せ内容を正しく入力して下さい。</p>
 
                     <button type='submit' id='button'>問合せる</button>
 
