ee059aff99d777d9e99fd1865e21218f56c586ae
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2026-03-20 10:06:27 +0900

    一通り完成

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,10 +5,26 @@ import { useState } from 'react';
 function Contact() {
 
     const { register, handleSubmit, formState: { errors }, reset } = useForm({ mode: "onChange" });
+    // ボタン制御のため追加
+    const [isSubmit, setIsSubmit] = useState(false); // 送信中か？
+    const [buttonText, setButtonText] = useState("問合せる"); // ボタンのテキスト表示変更用
 
     // 問合せクリック時（formサブミット時）
     const _onSubmit = async (data) => {
+
+        // 状態変更
+        setIsSubmit(true);
+        setButtonText("送信中・・・")
+
+        // sleep
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
         alert(`title=${data.title}, email=${data.email}, message=${data.message}`);
+        reset();
+
+        // 状態戻し
+        setButtonText("問合せる");
+        setIsSubmit(false);
     }
 
     return (
@@ -21,18 +37,36 @@ function Contact() {
                 <form className={styles.contactForm} onSubmit={handleSubmit(_onSubmit)}>
 
                     <label htmlFor='title'>お問合せタイトル</label>
-                    <input type='text' id='title' {...register("title")} />
-                    <p className={styles.errorMessage}>お問合せタイトルを正しく入力して下さい。</p>
+                    <input type='text' id='title' {...register("title", {
+                        required: "お問合せタイトルは必須です。",
+                        maxLength: {
+                            value: 10,
+                            message: "10文字以内で入力して下さい。"
+                        }
+                    })} />
+                    {errors.title && <p className={styles.errorMessage}>{errors.title.message}</p>}
 
                     <label htmlFor='email'>Email</label>
-                    <input type='text' id='email' {...register("email")} />
-                    <p className={styles.errorMessage}>Emailを正しく入力して下さい。</p>
+                    <input type='text' id='email' {...register("email", {
+                        required: "Emailは必須です。",
+                        pattern: {
+                            value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                            message: "Emailを正しい形式で入力して下さい。"
+                        }
+                    })} />
+                    {errors.email && <p className={styles.errorMessage}>{errors.email.message}</p>}
 
                     <label htmlFor='title'>お問合せ内容</label>
-                    <textarea id='message' rows={5} {...register("message")}></textarea>
-                    <p className={styles.errorMessage}>お問合せ内容を正しく入力して下さい。</p>
+                    <textarea id='message' rows={5} {...register("message", {
+                        required: "お問合せ内容は必須です。",
+                        maxLength: {
+                            value: 20,
+                            message: "20文字以内で入力して下さい。"
+                        }
+                    })}></textarea>
+                    {errors.message && <p className={styles.errorMessage}>{errors.message.message}</p>}
 
-                    <button type='submit' id='button'>問合せる</button>
+                    <button type='submit' id='button' disabled={isSubmit}>{buttonText}</button>
 
                 </form>
             </section>
